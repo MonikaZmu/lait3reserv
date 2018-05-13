fb112f656983d67416c5a67dec0f41eac20b6a6c
monikazmu <moniaaa1106@gmail.com>
2018-05-13 12:27:14 +0200

    dodana usługa na selenium, dodana usługa cucumbera

--- src/test/java/pl/lait/przychodnia3/Init.java
+++ src/test/java/pl/lait/przychodnia3/Init.java
@@ -1,23 +1,48 @@
 package pl.lait.przychodnia3;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Timestamp;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Init {
 
 	static WebDriver driver = null;
+	
+	static DesiredCapabilities cap = DesiredCapabilities.firefox();
 
 	public static WebDriver getDriver() {
 		System.setProperty("webdriver.gecko.driver",
 				"C:\\Users\\Monika\\Downloads\\geckodriver-v0.20.1-win64\\geckodriver.exe");
-		System.out.println("--------wewnąrz metody getDriver--------------");
+			  log("--------wewnąrz metody getDriver--------------");
+		
 
 		if (driver == null) {
-			System.out.println("--------wewnąrz IFa, FF jest już uruchomiony --------------");
-
-			driver = new FirefoxDriver();
+			log("--------wewnąrz IFa, FF jest już uruchomiony --------------");
+			
+			
+			
+			URL seleniumAdress = null;
+			try {
+				seleniumAdress = new URL("http://192.168.0.106:4444/wd/hub");
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//driver = new FirefoxDriver();
+			driver = new RemoteWebDriver(seleniumAdress, cap);
+					
 			driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 			driver.get("http://newtours.demoaut.com");
 			return driver;
@@ -51,5 +76,22 @@ public class Init {
 		System.out.print(tekst);
 		System.out.println("---");
 	}
+	
+	
+	public static void printScr(WebDriver driver) {
+    	
+    		Timestamp timestamp = new Timestamp(System.currentTimeMillis()); 
+    		Long milis = timestamp.getTime();
+    	
+    		WebDriver driver_tmp = new Augmenter().augment(driver);
+        File srcFile = ((TakesScreenshot)driver_tmp).getScreenshotAs(OutputType.FILE);
+        
+        try {
+			FileUtils.copyFile(srcFile, new File("target/screenshot-"+milis+".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+	}
 }
+
